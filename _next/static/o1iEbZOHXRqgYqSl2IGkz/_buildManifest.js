e1e0254eeab54d79d5230006280e34f027d0ff47
the-rahulk <the-rahulk@users.noreply.github.com>
2025-12-29 11:21:28 +0000

    deploy: 342d820bcb5b69a8d6a114181c82b39441fb6399

---
+++ _next/static/o1iEbZOHXRqgYqSl2IGkz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mayfair-fitness//_next/:path+",
+        "destination": "/mayfair-fitness/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
